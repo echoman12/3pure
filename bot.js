665167fa4f7cc6bca4774722483aeb61f94649be
echoman12 <45900323+echoman12@users.noreply.github.com>
2018-12-31 22:20:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 client.on('ready', () => {
-  client.user.setGame('3help.','https://www.twitch.tv/peery13');
+  client.user.setGame('Type 3help.','https://www.twitch.tv/peery13');
 });
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
